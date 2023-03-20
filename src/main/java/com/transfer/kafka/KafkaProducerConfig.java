1d036081fc217e2f27af333bc8a8df0f5d8d1957
christianus frederick <christanusfrederick@gmail.com>
2023-03-20 15:05:37 +0900

    Add zookeper setting

--- src/main/java/com/transfer/kafka/KafkaProducerConfig.java
+++ src/main/java/com/transfer/kafka/KafkaProducerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
@@ -18,16 +19,16 @@ public class KafkaProducerConfig {
     private String bootstrapAddress;
 
 
-    //    @Bean
-//    public ProducerFactory<String, String> producerFactory() {
-//        Map<String, Object> configProps = new HashMap<>();
-//        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-//        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-//        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-//        configProps.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, "20971520");
-//
-//        return new DefaultKafkaProducerFactory<>(configProps);
-//    }
+    @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, "20971520");
+
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
     private Map<String, Object> getKafkaConfigProps(){
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapAddress);
@@ -40,34 +41,6 @@ public class KafkaProducerConfig {
     public KafkaTemplate<String, String > kafkaTemplate() {
         return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(getKafkaConfigProps() ) );
     }
-//
-//    @Bean
-//    public ProducerFactory<String, Greeting> greetingProducerFactory() {
-//        Map<String, Object> configProps = new HashMap<>();
-//        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-//        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-//        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-//        return new DefaultKafkaProducerFactory<>(configProps);
-//    }
-//
-//    @Bean
-//    public KafkaTemplate<String, Greeting> greetingKafkaTemplate() {
-//        return new KafkaTemplate<>(greetingProducerFactory());
-//    }
-//
-//    @Bean
-//    public ProducerFactory<String, Object> multiTypeProducerFactory() {
-//        Map<String, Object> configProps = new HashMap<>();
-//        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-//        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-//        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-//        configProps.put(JsonSerializer.TYPE_MAPPINGS, "greeting:com.transfer.spring.kafka.Greeting, farewell:com.transfer.spring.kafka.Farewell");
-//        return new DefaultKafkaProducerFactory<>(configProps);
-//    }
-//
-//    @Bean
-//    public KafkaTemplate<String, Object> multiTypeKafkaTemplate() {
-//        return new KafkaTemplate<>(multiTypeProducerFactory());
-//    }
+
 
 }
