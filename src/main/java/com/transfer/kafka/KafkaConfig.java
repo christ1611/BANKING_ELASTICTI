eba4e258a6e1be3083f3576aa18116ee58b3d86f
christianus frederick <christanusfrederick@gmail.com>
2023-03-20 15:03:16 +0900

    Add zookeper setting

---
+++ src/main/java/com/transfer/kafka/KafkaConfig.java
@@ -0,0 +1,25 @@
+package com.transfer.kafka;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+@Configuration
+public class KafkaConfig {
+    @Value(value = "${kafka.bootstrap.server}")
+    private String bootstrapAddress;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapAddress);
+        return new KafkaAdmin(configs);
+    }
+
+
+
+}
